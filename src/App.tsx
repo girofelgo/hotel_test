ee079b1a088afa0929c29e33f4b26d54cf02e209
Tomasz Burzynski <girofelgo@gmail.com>
2021-11-12 00:42:40 +0100

    add star rating from mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Rating } from "@mui/material";
+
+type IValue = number | null;
 
 function App() {
+  const [value, setValue] = React.useState<IValue>(null);
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +14,13 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Rating
+          name="simple-controlled"
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        />
         <a
           className="App-link"
           href="https://reactjs.org"
